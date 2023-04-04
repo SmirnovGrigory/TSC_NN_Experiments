9a398ef8635113e863a3252da795145100669456
SmirnovGrigory <greg.smirnov2013@yandex.ru>
2023-04-04 23:18:06 +0300

    4 of april

---
+++ models.py
@@ -0,0 +1,428 @@
+from sktime.classification.hybrid import HIVECOTEV1
+
+from datetime import datetime
+
+import numpy as np
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import cross_val_predict
+from sklearn.utils import check_random_state
+
+from sktime.classification.base import BaseClassifier
+from sktime.classification.dictionary_based import ContractableBOSS
+from sktime.classification.interval_based import (
+    RandomIntervalSpectralEnsemble,
+    TimeSeriesForestClassifier,
+)
+from sktime.classification.shapelet_based import ShapeletTransformClassifier
+
+import torch
+import torch.nn as nn
+
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+
+import time
+
+
+class TSCDataset_MY(Dataset):
+    def __init__(self, X):
+        self.x = X
+
+    def __len__(self):
+        return len(self.x)
+
+    def __getitem__(self, i):
+        return torch.tensor([self.x[i]]).float()
+
+
+class HIVECOTEV1_MY(HIVECOTEV1):
+    _tags = {
+        "capability:multithreading": True,
+        "classifier_type": "hybrid",
+    }
+
+    def __init__(
+            self,
+            stc_params=None,
+            tsf_params=None,
+            rise_params=None,
+            cboss_params=None,
+            verbose=0,
+            n_jobs=1,
+            random_state=None,
+    ):
+        self.stc_params = stc_params
+        self.tsf_params = tsf_params
+        self.rise_params = rise_params
+        self.cboss_params = cboss_params
+
+        self.verbose = verbose
+        self.n_jobs = n_jobs
+        self.random_state = random_state
+
+        self.stc_weight_ = 0
+        self.tsf_weight_ = 0
+        self.rise_weight_ = 0
+        self.cboss_weight_ = 0
+
+        self.cnn_weight_ = 0
+
+        self._stc_params = stc_params
+        self._tsf_params = tsf_params
+        self._rise_params = rise_params
+        self._cboss_params = cboss_params
+        self._stc = None
+        self._tsf = None
+        self._rise = None
+        self._cboss = None
+
+        self.cnn = None
+
+        super(HIVECOTEV1, self).__init__()
+
+    def _fit(self, X, y):
+        """Fit HIVE-COTE 1.0 to training data.
+        Parameters
+        ----------
+        X : 3D np.array of shape = [n_instances, n_dimensions, series_length]
+            The training data.
+        y : array-like, shape = [n_instances]
+            The class labels.
+        Returns
+        -------
+        self :
+            Reference to self.
+        Notes
+        -----
+        Changes state by creating a fitted model that updates attributes
+        ending in "_" and sets is_fitted flag to True.
+        """
+        # Default values from HC1 paper
+        if self.stc_params is None:
+            self._stc_params = {"transform_limit_in_minutes": 120}
+        if self.tsf_params is None:
+            self._tsf_params = {"n_estimators": 500}
+        if self.rise_params is None:
+            self._rise_params = {"n_estimators": 500}
+        if self.cboss_params is None:
+            self._cboss_params = {}
+
+        # Cross-validation size for TSF and RISE
+        cv_size = 10
+        _, counts = np.unique(y, return_counts=True)
+        min_class = np.min(counts)
+        if min_class < cv_size:
+            cv_size = min_class
+
+        # Build STC
+        self._stc = ShapeletTransformClassifier(
+            **self._stc_params,
+            save_transformed_data=True,
+            random_state=self.random_state,
+            n_jobs=self._threads_to_use,
+        )
+        self._stc.fit(X, y)
+
+        if self.verbose > 0:
+            print("STC ", datetime.now().strftime("%H:%M:%S %d/%m/%Y"))  # noqa
+
+        # Find STC weight using train set estimate
+        train_probs = self._stc._get_train_probs(X, y)
+        train_preds = self._stc.classes_[np.argmax(train_probs, axis=1)]
+        self.stc_weight_ = accuracy_score(y, train_preds) ** 4
+
+        if self.verbose > 0:
+            print(  # noqa
+                "STC train estimate ",
+                datetime.now().strftime("%H:%M:%S %d/%m/%Y"),
+            )
+            print("STC weight = " + str(self.stc_weight_))  # noqa
+
+        # Build TSF
+        self._tsf = TimeSeriesForestClassifier(
+            **self._tsf_params,
+            random_state=self.random_state,
+            n_jobs=self._threads_to_use,
+        )
+        self._tsf.fit(X, y)
+
+        if self.verbose > 0:
+            print("TSF ", datetime.now().strftime("%H:%M:%S %d/%m/%Y"))  # noqa
+
+        # Find TSF weight using train set estimate found through CV
+        train_preds = cross_val_predict(
+            TimeSeriesForestClassifier(
+                **self._tsf_params, random_state=self.random_state
+            ),
+            X=X,
+            y=y,
+            cv=cv_size,
+            n_jobs=self._threads_to_use,
+        )
+        self.tsf_weight_ = accuracy_score(y, train_preds) ** 4
+
+        if self.verbose > 0:
+            print(  # noqa
+                "TSF train estimate ",
+                datetime.now().strftime("%H:%M:%S %d/%m/%Y"),
+            )
+            print("TSF weight = " + str(self.tsf_weight_))  # noqa
+
+        # Build RISE
+        self._rise = RandomIntervalSpectralEnsemble(
+            **self._rise_params,
+            random_state=self.random_state,
+            n_jobs=self._threads_to_use,
+        )
+        self._rise.fit(X, y)
+
+        if self.verbose > 0:
+            print("RISE ", datetime.now().strftime("%H:%M:%S %d/%m/%Y"))  # noqa
+
+        # Find RISE weight using train set estimate found through CV
+        train_preds = cross_val_predict(
+            RandomIntervalSpectralEnsemble(
+                **self._rise_params,
+                random_state=self.random_state,
+            ),
+            X=X,
+            y=y,
+            cv=cv_size,
+            n_jobs=self._threads_to_use,
+        )
+        self.rise_weight_ = accuracy_score(y, train_preds) ** 4
+
+        if self.verbose > 0:
+            print(  # noqa
+                "RISE train estimate ",
+                datetime.now().strftime("%H:%M:%S %d/%m/%Y"),
+            )
+            print("RISE weight = " + str(self.rise_weight_))  # noqa
+
+        # Build cBOSS
+        self._cboss = ContractableBOSS(
+            **self._cboss_params,
+            random_state=self.random_state,
+            n_jobs=self._threads_to_use,
+        )
+        self._cboss.fit(X, y)
+
+        # Find cBOSS weight using train set estimate
+        train_probs = self._cboss._get_train_probs(X, y)
+        train_preds = self._cboss.classes_[np.argmax(train_probs, axis=1)]
+        self.cboss_weight_ = accuracy_score(y, train_preds) ** 4
+
+        if self.verbose > 0:
+            print(  # noqa
+                "cBOSS (estimate included)",
+                datetime.now().strftime("%H:%M:%S %d/%m/%Y"),
+            )
+            print("cBOSS weight = " + str(self.cboss_weight_))  # noqa
+
+        return self
+
+    def _predict(self, X) -> np.ndarray:
+        """Predicts labels for sequences in X.
+        Parameters
+        ----------
+        X : 3D np.array of shape = [n_instances, n_dimensions, series_length]
+            The data to make predictions for.
+        Returns
+        -------
+        y : array-like, shape = [n_instances]
+            Predicted class labels.
+        """
+        rng = check_random_state(self.random_state)
+        return np.array(
+            [
+                self.classes_[int(rng.choice(np.flatnonzero(prob == prob.max())))]
+                for prob in self.predict_proba(X)
+            ]
+        )
+
+    def _predict_cnn(self, X) -> np.ndarray:
+        """Predicts labels for sequences in X.
+        Parameters
+        ----------
+        X : 3D np.array of shape = [n_instances, n_dimensions, series_length]
+            The data to make predictions for.
+        Returns
+        -------
+        y : array-like, shape = [n_instances]
+            Predicted class labels.
+        """
+        rng = check_random_state(self.random_state)
+        return np.array(
+            [
+                self.classes_[int(rng.choice(np.flatnonzero(prob == prob.max())))]
+                for prob in self._predict_proba_cnn(X)
+            ]
+        )
+
+    def _predict_proba(self, X) -> np.ndarray:
+        """Predicts labels probabilities for sequences in X.
+        Parameters
+        ----------
+        X : 3D np.array of shape = [n_instances, n_dimensions, series_length]
+            The data to make predict probabilities for.
+        Returns
+        -------
+        y : array-like, shape = [n_instances, n_classes_]
+            Predicted probabilities using the ordering in classes_.
+        """
+        dists = np.zeros((X.shape[0], self.n_classes_))
+
+        # Call predict proba on each classifier, multiply the probabilities by the
+        # classifiers weight then add them to the current HC1 probabilities
+        dists = np.add(
+            dists,
+            self._stc.predict_proba(X) * (np.ones(self.n_classes_) * self.stc_weight_),
+        )
+        dists = np.add(
+            dists,
+            self._tsf.predict_proba(X) * (np.ones(self.n_classes_) * self.tsf_weight_),
+        )
+        dists = np.add(
+            dists,
+            self._rise.predict_proba(X)
+            * (np.ones(self.n_classes_) * self.rise_weight_),
+        )
+        dists = np.add(
+            dists,
+            self._cboss.predict_proba(X)
+            * (np.ones(self.n_classes_) * self.cboss_weight_),
+        )
+
+        # Make each instances probability array sum to 1 and return
+        return dists / dists.sum(axis=1, keepdims=True)
+
+    def _predict_proba_cnn(self, X) -> np.ndarray:
+        """Predicts labels probabilities for sequences in X.
+        Parameters
+        ----------
+        X : 3D np.array of shape = [n_instances, n_dimensions, series_length]
+            The data to make predict probabilities for.
+        Returns
+        -------
+        y : array-like, shape = [n_instances, n_classes_]
+            Predicted probabilities using the ordering in classes_.
+        """
+        dists = np.zeros((X.shape[0], self.n_classes_))
+
+        # Call predict proba on each classifier, multiply the probabilities by the
+        # classifiers weight then add them to the current HC1 probabilities
+        dists = np.add(
+            dists,
+            self._stc.predict_proba(X) * (np.ones(self.n_classes_) * self.stc_weight_),
+        )
+        dists = np.add(
+            dists,
+            self._tsf.predict_proba(X) * (np.ones(self.n_classes_) * self.tsf_weight_),
+        )
+        dists = np.add(
+            dists,
+            self._rise.predict_proba(X)
+            * (np.ones(self.n_classes_) * self.rise_weight_),
+        )
+        dists = np.add(
+            dists,
+            self._cboss.predict_proba(X)
+            * (np.ones(self.n_classes_) * self.cboss_weight_),
+        )
+        dataset_MY = TSCDataset_MY(X)
+        dataloader_MY = DataLoader(dataset_MY, batch_size=dataset_MY.__len__(), shuffle=False)
+        softmax = nn.Softmax()
+        for _, X_MY in enumerate(dataloader_MY):
+            cnn_pred = softmax(self.cnn(X_MY)).detach().numpy()
+        dists = np.add(
+            dists,
+            cnn_pred
+            * (np.ones(self.n_classes_) * self.cnn_weight_),
+        )
+
+        # Make each instances probability array sum to 1 and return
+        return dists / dists.sum(axis=1, keepdims=True)
+
+    @classmethod
+    def get_test_params(cls, parameter_set="default"):
+        """Return testing parameter settings for the estimator.
+        Parameters
+        ----------
+        parameter_set : str, default="default"
+            Name of the set of test parameters to return, for use in tests. If no
+            special parameters are defined for a value, will return `"default"` set.
+            For classifiers, a "default" set of parameters should be provided for
+            general testing, and a "results_comparison" set for comparing against
+            previously recorded results if the general set does not produce suitable
+            probabilities to compare against.
+        Returns
+        -------
+        params : dict or list of dict, default={}
+            Parameters to create testing instances of the class.
+            Each dict are parameters to construct an "interesting" test instance, i.e.,
+            `MyClass(**params)` or `MyClass(**params[i])` creates a valid test instance.
+            `create_test_instance` uses the first (or only) dictionary in `params`.
+        """
+        from sklearn.ensemble import RandomForestClassifier
+
+        if parameter_set == "results_comparison":
+            return {
+                "stc_params": {
+                    "estimator": RandomForestClassifier(n_estimators=3),
+                    "n_shapelet_samples": 50,
+                    "max_shapelets": 5,
+                    "batch_size": 10,
+                },
+                "tsf_params": {"n_estimators": 3},
+                "rise_params": {"n_estimators": 3},
+                "cboss_params": {"n_parameter_samples": 5, "max_ensemble_size": 3},
+            }
+        else:
+            return {
+                "stc_params": {
+                    "estimator": RandomForestClassifier(n_estimators=1),
+                    "n_shapelet_samples": 5,
+                    "max_shapelets": 5,
+                    "batch_size": 5,
+                },
+                "tsf_params": {"n_estimators": 1},
+                "rise_params": {"n_estimators": 1},
+                "cboss_params": {"n_parameter_samples": 1, "max_ensemble_size": 1},
+            }
+
+
+class TimeSeriesCnnModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv1d(1, 32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv1d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool1d(2),
+
+            nn.Conv1d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv1d(128, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool1d(2),
+
+            nn.Conv1d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv1d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool1d(2),
+        )
+        self.linear = nn.Sequential(
+            nn.Linear(5888, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, 512),
+            nn.ReLU(),
+            nn.Linear(512, 5),
+            # nn.Softmax()
+        )
+
+    def forward(self, xb):
+        data = self.conv(xb)
+        data = torch.flatten(data, start_dim=1)
+        return self.linear(data)
---
+++ utils.py
@@ -0,0 +1,121 @@
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+
+import tqdm
+
+from sklearn.metrics import f1_score, balanced_accuracy_score, recall_score
+
+# def map_to_scores2(x):
+#     if x == 1:
+#         return torch.tensor([1., 0.])
+#     if x == 2:
+#         return torch.tensor([0., 1.])
+#
+#
+# class TSCDataset(Dataset):
+#     def __init__(self, X, y):
+#         self.x = X
+#         self.y = y
+#
+#     def __len__(self):
+#         return len(self.x)
+#
+#     def __getitem__(self, i):
+#         return torch.tensor([self.x.iloc[i][0]]).float(), map_to_scores2(float(self.y[i]))
+
+def map_to_scores(x):
+    if x == 0.0:
+        return torch.tensor([1., 0., 0., 0., 0.])
+    if x == 1.0:
+        return torch.tensor([0., 1., 0., 0., 0.])
+    if x == 2.0:
+        return torch.tensor([0., 0., 1., 0., 0.])
+    if x == 3.0:
+        return torch.tensor([0., 0., 0., 1., 0.])
+    if x == 4.0:
+        return torch.tensor([0., 0., 0., 0., 1.])
+
+class TSCDatasetNN(Dataset):
+    def __init__(self, X, y):
+        self.x = X
+        self.y = y
+
+    def __len__(self):
+        return len(self.x)
+
+    def __getitem__(self, i):
+        return torch.tensor([self.x[i]]).float(), map_to_scores(float(self.y[i]))
+
+class TSCDataset(Dataset):
+    def __init__(self, X, y):
+        self.x = X
+        self.y = y
+
+    def __len__(self):
+        return len(self.x)
+
+    def __getitem__(self, i):
+        return torch.tensor([self.x.iloc[i]]).float(), map_to_scores(float(self.y[i]))
+
+
+def get_f1_acc_recall(pred, target):
+    #print(pred)
+    #print(target)
+    rec = recall_score(target, pred, average='macro')
+    f1 = f1_score(target, pred, average='macro')
+    #print(f"RECALL SCORE: {rec}")
+    #print(f"ACCURACY SCORE: {balanced_accuracy_score(target, pred)}")
+    #print(f"F1 SCORE: {f1}")
+    return rec, f1
+
+
+def evaluate(model, dataloader_test):
+    lst = []
+    targets = []
+    model.eval()
+    with torch.no_grad():
+        for X, y in dataloader_test:
+            pred = model(X)
+            #print(pred)
+            lst.append((torch.argmax(pred, dim=1)).numpy())
+            #print(lst)
+            targets.append(torch.argmax(y, dim=1).numpy())
+    lst = np.concatenate(lst)
+    #print(lst)
+    #lst = [map_to_scores(x) for x in lst[0]]
+    targets = np.concatenate(targets)
+    #print(lst)
+    #print(targets)
+    return get_f1_acc_recall(lst, targets)
+
+
+def train_loop(model, epochs, optim, loss_fn, dataloader_train, dataloader_test, n_batches):
+    best_rec = 0
+    best_f1 = 0
+    for epoch in range(epochs):
+        print("EPOCH: ", epoch)
+        for i, (X, y) in tqdm.notebook.tqdm(enumerate(dataloader_train), total=n_batches):
+            # print(X, y)
+            pred = model(X)
+            # print(pred)
+            loss = loss_fn(pred, y)
+            optim.zero_grad()
+            loss.backward()
+            #scheduler.step(loss)
+            optim.step()
+
+            if i % 10 == 0:
+                print(f"train loss: {loss.item():>5f}")
+
+        rec, f1 = evaluate(model, dataloader_test)
+        print("RECALL: ", rec)
+        print("F1 SCORE: ", f1)
+        if rec > best_rec: best_rec = rec
+        if f1 > best_f1: best_f1 = f1
+        if f1 < best_f1 and rec < best_rec:
+            print("EARLY STOPPING")
+            return
+        model.train()
+
